641e5e2ab5d828d1ef7e1a68f8bbc825a201f46e
Flo Faber <florian@flofaber.com>
2023-06-24 18:56:23 +0200

    added copyright

--- doc/www/templates/footer.html.php
+++ doc/www/templates/footer.html.php
@@ -1,4 +1,5 @@
     </div>
+    <div style="text-align: center; padding: 30px;">&copy; 2023 Florian Faber</div>
     <script>hljs.highlightAll();</script>
     <script type="text/javascript" src="/js/main.js"></script>
   </body>
