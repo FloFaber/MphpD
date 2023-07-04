abd373e33e7f6982b16552bc79811631ce670793
Flo Faber <github.com@flofaber.com>
2023-07-04 15:57:43 +0200

    Deleted www/

--- doc/www/templates/footer.html.php
+++
@@ -1,6 +0,0 @@
-    </div>
-    <div style="text-align: center; padding: 30px;">&copy; 2023 Florian Faber</div>
-    <script>hljs.highlightAll();</script>
-    <script type="text/javascript" src="/js/main.js"></script>
-  </body>
-</html>
